41636e5fa9160cb3edbaa6c83f917773025dba58
caiosantosss <caio.santos@msn.com>
2022-12-03 22:55:37 +0900

    add scroll for navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import { useState } from "react";
 import useMediaQuery from './hooks/useMediaQuery';
 import Navbar from './scenes/Navbar';
+import DotGroup from '/scenes/DotGroup';
 
 function App() {
   const [selectedPage, setSelectedPage] = useState('home');
   const isAboveMediumScreens = useMediaQuery({ minWidth: 1060 });
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isTop = window.scrollY < 0;
+      if (isTop !== isTopOfPage) setIsTopOfPage(isTop);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [isTopOfPage]);
+
 
   return <div className="app bg-deep-blue">
     <Navbar
+      isTopOfPage={isTopOfPage}
       selectedPage={selectedPage}
       setSelectedPage={setSelectedPage}
     />
+    <div className="w-5/6 mx-auto md:h-full">
+      {isAboveMediumScreens && (
+        <DotGroup
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
+      )}
+    </div>
   </div>
 }
 
