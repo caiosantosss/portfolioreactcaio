7194854d25691e044db67c2d07c68a58aeda0c21
caiosantosss <caio.santos@msn.com>
2022-12-05 21:51:58 +0900

    add profilepic to landing

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import useMediaQuery from '../hooks/useMediaQuery';
 import { motion } from 'framer-motion';
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import Image from '../assets/profilepic1.jpg'
 
 const Landing = ({ setSelectedPage }) => {
   const isAboveMediumScreens = useMediaQuery('(min-width: 1024px)');
@@ -9,6 +10,30 @@ const Landing = ({ setSelectedPage }) => {
   return (
     <section id='home' className='md:flex md:justify-between md:items-center md:h-full gap-16 py-10'>
 
+      {/* image section */}
+      <div className='md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32'>
+        {isAboveMediumScreens ? (
+          <div
+            className='relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]'
+          >
+            <img
+              alt='profile'
+              className='hover:filter hover:saturate-200 transition duration-500 ease-in-out z-10 w-full max-w-[400px] md:max-w-[600px] rounded-t-[400px] shadow-2xl'
+              src={Image}
+            />
+          </div>
+        ) : (
+          <div>
+            <img
+              alt='profile'
+              className='hover:filter hover:saturate-200 transition duration-500 ease-in-out z-10 w-full max-w-[400px] md:max-w-[600px] rounded-t-[400px] shadow-2xl'
+              src={Image}
+            />
+          </div>
+        )}
+
+        
+      </div>
     </section>
   )
 }
