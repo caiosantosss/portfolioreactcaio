fcdf0574880eae1d47ee08e4aece12c20560e279
caiosantosss <caio.santos@msn.com>
2022-12-06 22:47:34 +0900

    add motiondiv to button contactme

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -59,6 +59,40 @@ const Landing = ({ setSelectedPage }) => {
             creating beautiful and functional websites.
           </p>
         </motion.div>
+
+        {/* call to actions */}
+        <motion.div
+          className='flex mt-5 justify-center md:justify-start '
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5, ease: 'easeInOut' }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <AnchorLink
+            className='bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold
+              hover:bg-blue hover:text-white transition duration-500 ease-in-out'
+            href='#contact'
+            onClick={() => setSelectedPage('contact')}
+          >
+            Contact Me
+          </AnchorLink>
+          <AnchorLink
+            className='rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5'
+            href='#contact'
+            onClick={() => setSelectedPage('contact')}
+          >
+            <div
+              className='bg-deep-blue hover:text-red transition duration-500 h-full w-full flex items-center
+                justify-center font-playfair px-10'
+            >
+              Lets Talk
+            </div>
+          </AnchorLink>
+        </motion.div>
       </div>
     </section>
   )
