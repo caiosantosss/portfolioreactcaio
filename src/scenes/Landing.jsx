d318548b670ccdeacee3f7d4a8164575c8fbfa9e
caiosantosss <caio.santos@msn.com>
2022-12-07 22:31:46 +0900

    add socialmedia on landing

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -93,6 +93,20 @@ const Landing = ({ setSelectedPage }) => {
             </div>
           </AnchorLink>
         </motion.div>
+
+        <motion.div
+          className='flex mt-5 justify-center md:justify-start '
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5, ease: 'easeInOut' }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <SocialMediaIcons />
+        </motion.div>
       </div>
     </section>
   )
