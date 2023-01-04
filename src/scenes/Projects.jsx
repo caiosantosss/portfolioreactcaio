8377cdce8c7553d8bf753a1ce158df1737c57012
caiosantosss <caio.santos@msn.com>
2023-01-04 18:43:23 +0900

    add projects

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -1,10 +1,47 @@
 import LineGradient from "../components/LineGradient";
 import { motion } from "framer-motion";
 
+const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const projectVariant = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+  },
+};
+
+const Project = ({ title }) => {
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-gradient-to-r from-red to-blue z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+  const projectTitle = title.split(" ").join("-").toLowerCase();
+
+  return (
+    <motion.div variants={projectVariant} className="relative">
+      <div className={overlayStyles}>
+        <p className="text-2xl font-playfair">
+          {title}
+        </p>
+        <p className="mt-7">
+          Lorem ipsum dolor sit amet consectetur
+        </p>
+      </div>
+      <img src={`../assets/${projectTitle}.jpeg`} alt={title} />
+    </motion.div>
+  )
+}
+
+
 const Projects = () => {
   return (
     <section id="projects" className="pt-48 pb-48">
-      {/* HEADINGS */}\
+      {/* HEADINGS */}
       <motion.div
         className='md:w-2/4 mx-auto text-center'
         initial="hidden"
@@ -28,6 +65,40 @@ const Projects = () => {
           lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
         </p>
       </motion.div>
+
+      {/* PROJECTS */}
+      <div className="flex justify-center">
+        <motion.div
+        className='sm:grid sm:grid-cols-3'
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={container}
+        >
+          <div
+            className='flex justify-center text-center items-center p-10 bg-red max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold'
+          >
+            Test projects
+          </div>
+          <Project title='Project 1' />
+          <Project title='Project 2' />
+
+          {/* ROW 2 */}
+          <Project title='Project 3' />
+          <Project title='Project 4' />
+          <Project title='Project 5' />
+
+          {/* ROW 3 */}
+          <Project title='Project 6' />
+          <Project title='Project 7' />
+
+          <div
+            className='flex justify-center text-center items-center p-10 bg-blue max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold'
+          >
+            Test final projects
+          </div>
+        </motion.div>
+      </div>
     </section>
   );
 };
